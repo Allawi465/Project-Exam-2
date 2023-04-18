92074a27d29d7fccd16dbbc8cb67ad3fddaa3f22
Allawi <allawi465@gmail.com>
2023-04-18 04:37:45 +0200

    added pages and layout to navbar

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+function RouteNotFound() {
+    return <div className='text-white'>
+        <Helmet>
+            <title>Holidaze - 404 Page Not Found</title>
+            <meta name="description" content="Page not found. Please try again." />
+            <meta name="keywords" content="404, page not found, Holidaze" />
+        </Helmet>
+            <div >
+            pages not Found
+            </div>
+    </div>;
+}
+
+export default RouteNotFound;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+
+function Home() {
+    return (
+        <div>
+            <Helmet>
+                <title>Holidaze: Plan Your Next Adventure with Ease</title>
+                <meta
+                    name="description" content="Holidaze makes vacation planning stress-free. Find, book, and manage your rental with ease and enjoy your trip without worries." />
+                <meta name="keywords" content="travel, vacation, holiday, rental, book, rent out, host, homes, apartments, bookings, reviews" />
+            </Helmet>
+            <div>
+                hei
+            </div>
+        </div>
+    );
+}
+
+export default Home;
