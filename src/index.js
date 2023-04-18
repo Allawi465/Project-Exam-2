d36a491c155869dd4e0152a6ec750dec97ee0f11
Allawi <allawi465@gmail.com>
2023-04-18 03:28:48 +0200

    added GlobalStyle and theme to index.js and working on app.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Theme from './style/theme';
+import GlobalStyle from './style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Theme>
+      <GlobalStyle />
+      <App />
+    </Theme>
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
