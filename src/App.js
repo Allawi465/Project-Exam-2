787a0a55fe8376fe1672ff3b61daab5f7308c330
Allawi <allawi465@gmail.com>
2023-04-18 04:41:41 +0200

    added HelmetProvider, Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './index.scss';
+import { Layout } from './components//index';
+import { Home, RouteNotFound } from './pages';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        hei
-      </h1>
+    <div>
+      <HelmetProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<RouteNotFound />} />
+          </Route>
+        </Routes>
+      </HelmetProvider>
     </div>
   );
 }
