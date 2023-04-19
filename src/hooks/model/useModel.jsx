526f42cb12562e2d0b63e378a8581ae0b8133e7f
Allawi <allawi465@gmail.com>
2023-04-19 18:42:39 +0200

    added hooks for use model

---
+++ src/hooks/model/useModel.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+export default function useModal() {
+  const [showLoginModel, setShowLoginModel] = useState(false);
+  const [showSignUpModel, setShowSignUpModel] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
+
+  const handleCloseSettings = () => {
+    setShowSettings(false);
+  };
+
+  const handleOpenSettings = () => {
+    setShowSettings(true);
+  };
+
+  const handleLoginModel = () => {
+    setShowLoginModel(true);
+    setShowSignUpModel(false) // close the sign up model if it's open
+  };
+
+  const handleCloseLoginModel = () => {
+    setShowLoginModel(false);
+  };
+
+  const handleSignUpModel = () => {
+    setShowSignUpModel(true);
+    setShowLoginModel(false); // close the login model if it's open
+  };
+
+  const handleCloseSignUpModel = () => {
+    setShowSignUpModel(false);
+  };
+
+  return {
+    showLoginModel,
+    showSignUpModel,
+    handleLoginModel,
+    handleCloseLoginModel,
+    handleSignUpModel,
+    handleCloseSignUpModel,
+    showSettings,
+    handleCloseSettings,
+    handleOpenSettings
+  };
+}
