1155f9fed0a25e357a97a0ed1539c41c143e8aca
Allawi <allawi465@gmail.com>
2023-04-28 05:19:42 +0200

    added ApiProvider as wrapper

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import './index.scss';
 import { Layout } from './components//index';
 import { Home, VenueById, Profile, RouteNotFound } from './pages';
 import { HelmetProvider } from 'react-helmet-async';
+import ApiProvider from './api/auth';
 
 function App() {
   return (
     <div>
       <HelmetProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="venue/:id" element={<VenueById />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="*" element={<RouteNotFound />} />
-          </Route>
-        </Routes>
+        <ApiProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="venue/:id" element={<VenueById />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="*" element={<RouteNotFound />} />
+            </Route>
+          </Routes>
+        </ApiProvider>
       </HelmetProvider>
     </div>
   );
