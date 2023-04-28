a1def33fc8522112d49c244492e9d3eaf4b5db79
Allawi <allawi465@gmail.com>
2023-04-28 05:17:06 +0200

    created form with yup and api by submit

--- src/form/login/index.jsx
+++ src/form/login/index.jsx
@@ -1,29 +1,67 @@
+import React, { useState, useContext } from 'react';
 import { FloatingLabel, Form } from 'react-bootstrap';
 import LoadingButton from '../../components/LoadingButton';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import LoginSchema from './schema';
+import { AuthContext } from '../../api/auth';
 
-function LoginForm() {
+
+function LoginForm({ onClose }) {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors, isValid },
+    } = useForm({
+        resolver: yupResolver(LoginSchema),
+    });
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+
+    const { login, isLoading } = useContext(AuthContext);
+
+    async function OnFormSubmit(formData) {
+        console.log(formData);
+        setFormSubmitted(true);
+        const user = await login(formData);
+        reset();
+        if (user && user.errors) {
+            setErrorMessage(user.errors[0].message);
+        } else {
+            onClose();
+        }
+    }
 
     return (
         <>
-            <form>
-
-                <FloatingLabel label="Email address" name="email" className="my-3 text-black">
+            <form onSubmit={handleSubmit(OnFormSubmit)}>
+                {errorMessage && <p style={{ color: 'var(--color-red)' }}>{errorMessage}</p>}
+                <FloatingLabel label="Email address" name="email" className="my-3">
                     <Form.Control
+                        {...register('email')}
                         type="email"
                         name="email"
-                        placeholder="name@example.com" />
+                        placeholder="name@stud.noroff.no"
+                    />
+                    <p className='form-text mx-1'>{errors.email?.message}</p>
                 </FloatingLabel>
+
                 <FloatingLabel label="Password"
-                    className="my-3 text-black">
+                    className="my-3">
                     <Form.Control
+                        {...register('password')}
                         type="password"
                         name="password"
                         placeholder="password"
                     />
+                    <p className='form-text mx-1'>{errors.password?.message}</p>
                 </FloatingLabel>
 
                 <div className="d-grid">
-                 <LoadingButton buttonText="Login" />
+                    <LoadingButton buttonText="Login" type="submit" isValid={isValid} loading={isLoading} />
                 </div>
             </form>
         </>
--- src/form/sign-up/index.jsx
+++ src/form/sign-up/index.jsx
@@ -1,54 +1,99 @@
+import React, { useState, useContext, useEffect } from 'react';
 import { FloatingLabel, Form } from 'react-bootstrap';
 import LoadingButton from '../../components/LoadingButton';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import schema from './register';
+import { AuthContext } from '../../api/auth';
 
-function SignUpForm() {
+function SignUpForm({ onSignUpClick }) {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors, isValid },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const { signUp, isLoading } = useContext(AuthContext);
+
+
+    async function OnFormSubmit(formData) {
+        console.log(formData);
+        setFormSubmitted(true);
+        const user = await signUp(formData);
+        reset();
+        if (user && user.errors) {
+            setErrorMessage(user.errors[0].message);
+        } else {
+            onSignUpClick()
+        }
+    }
 
     return (
+
         <>
-            <form>
+            <form onSubmit={handleSubmit(OnFormSubmit)}>
+                {errorMessage && <p style={{ color: 'var(--color-red)' }}>{errorMessage}</p>}
                 <FloatingLabel label="Name"
-                    className="my-3 text-black">
+                    className="my-3">
                     <Form.Control
+                        {...register('name')}
                         type="text"
                         name="name"
                         placeholder="name"
                     />
+                    <p className='form-text mx-1'>{errors.name?.message}</p>
                 </FloatingLabel>
 
-                <FloatingLabel label="Email address" name="email" className="my-3 text-black">
+                <FloatingLabel label="Email address" name="email" className="my-3">
                     <Form.Control
+                        {...register('email')}
                         type="email"
                         name="email"
-                        placeholder="name@example.com" />
+                        placeholder="name@stud.noroff.no"
+                    />
+                    <p className='form-text mx-1'>{errors.email?.message}</p>
                 </FloatingLabel>
 
                 <FloatingLabel label="Avatar"
-                    className="my-3 text-black">
+                    className="my-3">
                     <Form.Control
+                        {...register('avatar')}
                         type="url"
                         name="avatar"
                         placeholder="avatar"
                     />
+                    <p className='form-text mx-1'>{errors.avatar?.message}</p>
                 </FloatingLabel>
 
                 <FloatingLabel label="Password"
-                    className="my-3 text-black">
+                    className="my-3">
                     <Form.Control
+                        {...register('password')}
                         type="password"
                         name="password"
                         placeholder="password"
                     />
+                    <p className='form-text mx-1'>{errors.password?.message}</p>
                 </FloatingLabel>
 
-                <div className='d-flex mb-3'>
-                    <label htmlFor="switch" className='me-3'>Become a host</label>
+                <div className='d-flex mb-3 mx-1'>
+                    <label htmlFor="switch" className='me-2' >Become a host</label>
                     <Form.Check
+                        {...register('venueManager')}
                         type="switch"
                         id="custom-switch"
+                        defaultChecked={true}
+                        name="venueManager"
                     />
                 </div>
+
                 <div className="d-grid">
-                <LoadingButton buttonText="Sign up"/>
+                    <LoadingButton buttonText="Sign up" type="submit" isValid={isValid} Loading={isLoading} />
                 </div>
             </form>
         </>
