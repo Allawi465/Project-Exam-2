3d5451a029e85272797386b4d024fa969cb70d4b
Allawi <allawi465@gmail.com>
2023-05-02 12:56:06 +0200

    made useGetApi and useApiActions

--- src/hooks/api/useApiActions.jsx
+++ src/hooks/api/useApiActions.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { fetchWToken } from '../../api/headers';
+import { API_SOCIAL_URL } from '../../api/constants';
 
-function useApi(baseUrl) {
+function useApiActions() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setError] = useState(false);
@@ -10,9 +11,10 @@ function useApi(baseUrl) {
     try {
       setIsLoading(true);
       setError(false);
-      const response = await fetchWToken(`${baseUrl}${path}`, options);
-      console.log(response);
-      const data = await response.json();
+      const { method, body } = options;
+      const url = `${API_SOCIAL_URL}${path}`;
+      const fetchedData = await fetchWToken(url, { method, body});
+      const data = await fetchedData.json();
       console.log(data);
       if (data.errors) {
         setError(data.errors[0].message);
@@ -33,4 +35,4 @@ function useApi(baseUrl) {
   return { data, isLoading, isError, fetchData };
 }
 
-export default useApi;
+export default useApiActions;
---
+++ src/hooks/api/useGetApi.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { API_SOCIAL_URL } from '../../api/constants';
+import { fetchWToken } from '../../api/headers';
+
+function useGetApi(path) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setError(false);
+        const url = `${API_SOCIAL_URL}${path}`;
+        const fetchedData = await fetchWToken(url);
+        const data = await fetchedData.json();
+        console.log(data);
+        if (data.errors) {
+          setError(data.errors[0].message);
+          return { isError: data.errors[0].message };
+        } else {
+          setData(data);
+          return { data };
+        }
+      } catch (error) {
+        console.log(error);
+        setError(true);
+        return { isError: true, error };
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getData();
+  }, [path]);
+  return { data, isLoading, isError };
+}
+
+
+export default useGetApi ;
