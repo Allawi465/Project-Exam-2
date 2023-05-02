668e99fbf424858f34f41f35fa4a2803d0052f58
Allawi <allawi465@gmail.com>
2023-05-02 12:54:43 +0200

    updated the ApiProvider

--- src/api/index.js
+++ src/api/index.js
@@ -1,56 +1,27 @@
 import React, { useState, createContext } from 'react';
 import { remove } from '../utils/localStorage';
-import { API_SOCIAL_URL } from "./constants";
-import useApi from '../hooks/api/useApi';
 
 export const AuthContext = createContext();
 
 const ApiProvider = ({ children }) => {
-  const baseUrl = API_SOCIAL_URL;
+  const [data, setData] = useState(false);
   const [dataLogin, setDataLogin] = useState(false);
-  const { data, isLoading, fetchData, isError } = useApi(baseUrl);
-
-  const login = async (userInput) => {
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(userInput),
-    };
-    const { data, isError } = await fetchData('/auth/login', options);
-    setDataLogin(data);
-    return { data, isError };
-  };
-
-  const post = async (userInput, path = '') => {
-    const options = {
-      method: 'POST',
-      body: JSON.stringify(userInput),
-    };
-    const { data, isError } = await fetchData(path, options);
-    return { data, isError };
-  };
-
-  const read = async (path = '') => {
-    const { data, isError } = await fetchData(path);
-    return { data, isError };
-  };
 
   const logout = () => {
     remove('token');
     remove('user');
+    remove('avatar');
     setDataLogin(null);
   };
 
   return (
     <AuthContext.Provider
       value={{
-        post,
-        login,
-        logout,
-        read,
-        isLoading,
         data,
-        isError,
+        setData,
         dataLogin,
+        setDataLogin,
+        logout,
       }}
     >
       {children}
