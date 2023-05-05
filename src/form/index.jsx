5549000cc8c228d2ee63e4d9b1377c64d0e45de8
Allawi <allawi465@gmail.com>
2023-05-05 17:57:40 +0200

    added index to export files from it

---
+++ src/form/index.jsx
@@ -0,0 +1,4 @@
+export { default as LoginForm } from './login';
+export { default as SignUpForm } from './sign-up';
+export { default as ChangeAvatarForm } from './settings';
+export { default as Search } from './search';
