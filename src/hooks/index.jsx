6fd7599a9ebed0f87ad0757b70b3b3f33031efaa
Allawi <allawi465@gmail.com>
2023-05-05 17:59:29 +0200

    added index file to export the functions from it

---
+++ src/hooks/index.jsx
@@ -0,0 +1,4 @@
+export { default as useGetApi } from './api/useGetApi';
+export { default as useApiActions } from './api/useApiActions';
+export { default as UseModel } from './model';
+export { default as useCarousel } from './carousel';
