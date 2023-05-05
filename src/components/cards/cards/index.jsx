f9baaa5521a2c7ac37239ea2620eca0d092e92b3
Allawi <allawi465@gmail.com>
2023-05-05 17:51:20 +0200

    change path to imported functions

--- src/components/cards/cards/index.jsx
+++ src/components/cards/cards/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+  import { Link } from 'react-router-dom';
 import { Card, Col } from 'react-bootstrap';
 import { useGetApi } from '../../../hooks/index';
 import { Loading, ErrorMessage } from '../../index';
