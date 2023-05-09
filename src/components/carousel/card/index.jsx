54a34c69a5171b9c00a6dd6b31916747337e13be
Allawi <allawi465@gmail.com>
2023-05-09 02:01:41 +0200

    added extra div to fix the thumbnails images

--- src/components/carousel/card/index.jsx
+++ src/components/carousel/card/index.jsx
@@ -14,35 +14,55 @@ function VenueCarousel({ media, name }) {
 
   return (
     <div className="venue-img">
-      {media.length ? (
-        <Carousel
-          activeIndex={currentImage}
-          onSelect={handleSelect}
-          interval={null}
-          controls={false}
-          indicators={imagesLength > 1}
-        >
-          {media.map((image, index) => (
-            <Carousel.Item key={index}>
+      <div className="venue-img-container">
+        {media.length ? (
+          <Carousel
+            activeIndex={currentImage}
+            onSelect={handleSelect}
+            interval={null}
+            controls={false}
+            indicators={imagesLength > 1}
+          >
+            {media.map((image, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  src={image}
+                  alt={`${name} ${index} of ${imagesLength}`}
+                  onError={(e) => {
+                    e.target.src = defaultImages;
+                  }}
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        ) : (
+          <img src={defaultImages} alt={`${name} default`} />
+        )}
+        <ButtonsCarousel
+          currentImage={currentImage}
+          media={media}
+          handleScrollLeft={handleScrollLeft}
+          handleScrollRight={handleScrollRight}
+        />
+      </div>
+      <div className="venue-img-thumbnails">
+        {media.length > 1 && (
+          <div className="venue-img-thumbnails">
+            {media.map((image, index) => (
               <img
+                key={index}
                 src={image}
-                alt={`${name} ${index} of ${imagesLength}`}
+                alt={`${name} ${index}`}
+                className={currentImage === index ? 'active' : ''}
+                onClick={() => handleSelect(index)}
                 onError={(e) => {
                   e.target.src = defaultImages;
                 }}
               />
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      ) : (
-        <img src={defaultImages} alt={`${name} default`} />
-      )}
-      <ButtonsCarousel
-        currentImage={currentImage}
-        media={media}
-        handleScrollLeft={handleScrollLeft}
-        handleScrollRight={handleScrollRight}
-      />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
