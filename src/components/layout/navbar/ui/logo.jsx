0f13c8deb2f2f9f001e8ceda01d765905c35d44e
Allawi <allawi465@gmail.com>
2023-05-09 02:02:23 +0200

    edited the logo

--- src/components/layout/navbar/ui/logo.jsx
+++ src/components/layout/navbar/ui/logo.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from '../../../../images/logo.jpg';
+import logo from '../../../../images/logo.png';
 
 function Logo() {
   return (
-    <Navbar.Brand as={Link} to="/" className="pb-0">
+    <Navbar.Brand as={Link} to="/" className="logo my-0 pt-0">
       <img
         src={logo}
-        width="60"
-        height="60"
+        width="50"
+        height="50"
         className="d-inline-block align-top"
         alt="Holidaze logo"
       />
+      Holidaze
     </Navbar.Brand>
   );
 }
