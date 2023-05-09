b7ed988e1e4fca24b64cdd585a4a3dcc327c39a0
Allawi <allawi465@gmail.com>
2023-05-09 02:05:05 +0200

    edited the colors on btn

--- src/style/buttons.js
+++ src/style/buttons.js
@@ -2,47 +2,44 @@ import styled from 'styled-components';
 
 export const LoginBtn = styled.button`
   background: transparent;
-  padding: 6px 10px;
+  padding: 0.375rem 0.75rem;
   color: #000000;
   border: 1px solid transparent;
   line-height: 1.5;
   border-radius: 0.25rem;
   font-weight: 600;
   font-size: 14px;
-  transition: all color 300ms ease, background-color 300ms ease;
+  transition: all 0.2s ease-in-out;
+  :hover {
+    background: ${(props) => props.theme.color.lightpink};
+    color: ${(props) => props.theme.color.darkPurple};
+  }
+`;
+
+export const SignUpBtn = styled(LoginBtn)`
+  background: ${(props) => props.theme.color.purple};
+  color: #ffffff;
+`;
+
+export const ModelBtn = styled(SignUpBtn)`
+  background: ${(props) => props.theme.color.lightBlack};
+`;
+
+export const ModelLoadingBtn = styled(ModelBtn)`
+  background: ${(props) => props.theme.color.purple};
+`;
+
+export const HeroBtn = styled(SignUpBtn)`
+  background: #fdfdfd;
+  color: ${(props) => props.theme.color.purple};
+  border-radius: 49px;
   :hover {
     background: ${(props) => props.theme.color.purple};
     color: #ffffff;
   }
 `;
 
-export const SignUpBtn = styled(LoginBtn)`
-  background: ${(props) => props.theme.color.lightBlack};
-  color: #ffffff;
-`;
-
-export const ModelBtn = styled(SignUpBtn)`
-  padding: 0.375rem 0.75rem;
-  &:hover {
-    background: #1c1c1d;
-  }
-`;
-
-export const ModelLoadingBtn = styled(ModelBtn)`
-  background: ${(props) => props.theme.color.purple};
-  &:hover {
-    background: #413563;
-  }
-`;
-
-export const HeroBtn = styled(LoginBtn)`
-  background: #fdfdfd;
-  padding: 0.375rem 0.75rem;
-  color: ${(props) => props.theme.color.purple};
-  border-radius: 49px;
-`;
-
-export const BookBtn = styled(LoginBtn)`
+export const BookBtn = styled(SignUpBtn)`
   background: ${(props) => props.theme.color.purple};
   color: #ffffff;
   width: 156px;
