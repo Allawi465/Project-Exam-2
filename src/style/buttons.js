23dc133eeb6ce28c17d9c7ac2cdb4bbbe956e21f
Allawi <allawi465@gmail.com>
2023-05-15 14:36:52 +0200

    added more style to the pages

--- src/style/buttons.js
+++ src/style/buttons.js
@@ -1,48 +1,54 @@
 import styled from 'styled-components';
 
 export const LoginBtn = styled.button`
-  background: transparent;
+  background: ${(props) => props.theme.color.purple};
   padding: 0.375rem 0.75rem;
-  color: #000000;
+  color: #ffffff;
   border: 1px solid transparent;
   line-height: 1.5;
   border-radius: 0.25rem;
-  font-weight: 600;
-  font-size: 14px;
+  font-size: 16px;
   transition: all 0.2s ease-in-out;
   :hover {
-    background: ${(props) => props.theme.color.lightpink};
-    color: ${(props) => props.theme.color.darkPurple};
+    border: 1px solid ${(props) => props.theme.color.purple};
+    background: transparent;
+    color: #000000;
   }
 `;
 
-export const SignUpBtn = styled(LoginBtn)`
-  background: ${(props) => props.theme.color.purple};
-  color: #ffffff;
-`;
-
-export const ModelBtn = styled(SignUpBtn)`
+export const ModelBtn = styled(LoginBtn)`
   background: ${(props) => props.theme.color.lightBlack};
+  :hover {
+    border: 1px solid ${(props) => props.theme.color.lightBlack};
+    background: #ffffff;
+    color: #000000;
+  }
 `;
 
 export const ModelLoadingBtn = styled(ModelBtn)`
   background: ${(props) => props.theme.color.purple};
-`;
-
-export const HeroBtn = styled(SignUpBtn)`
-  background: #fdfdfd;
-  color: ${(props) => props.theme.color.purple};
-  border-radius: 49px;
   :hover {
     background: ${(props) => props.theme.color.purple};
     color: #ffffff;
   }
 `;
 
-export const BookBtn = styled(SignUpBtn)`
+export const HeroBtn = styled(LoginBtn)`
+  background: #fdfdfd;
+  color: ${(props) => props.theme.color.purple};
+  border-radius: 49px;
+  :hover {
+    background: ${(props) => props.theme.color.purple};
+    color: #ffffff;
+    border: 1px solid transparent;
+  }
+`;
+
+export const BookBtn = styled(LoginBtn)`
   background: ${(props) => props.theme.color.purple};
   color: #ffffff;
-  width: 156px;
+  max-width: 156px;
+  width: 100%;
   border-radius: 72px;
   font-size: 20px;
   height: 48px;
