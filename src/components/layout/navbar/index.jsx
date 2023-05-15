0033456e162ca7539075b2b4680d04534a103572
Allawi <allawi465@gmail.com>
2023-05-15 14:26:15 +0200

    added jsDocs

--- src/components/layout/navbar/index.jsx
+++ src/components/layout/navbar/index.jsx
@@ -1,8 +1,22 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Navbar } from 'react-bootstrap';
+import logo from '../../../images/logo.png';
 import { AuthContext } from '../../../api';
 import { load } from '../../../utils/localStorage';
-import { UnAuthUi, Logo, UserNavbar } from './ui/index';
+import { UnAuthUi, UserNavbar } from './ui/index';
+
+/**
+ * Renders the navigation bar component
+ * @component
+ * @property {function} AuthContext getting the authentication state from AuthContext
+ * @property {function} load authentication state from local Storage
+ * @property {function} UserNavbar render the User navbar from a component if authentication state is true
+ * @property {function} UnAuthUi render the global navbar from a component if authentication state is false
+ * @returns {React.ReactElement} return NavBar component
+ * @example
+ * <NavBar />
+ */
 
 function NavBar() {
   const { dataLogin } = useContext(AuthContext);
@@ -16,7 +30,16 @@ function NavBar() {
       style={{ background: 'white' }}
     >
       <Container style={{ maxWidth: '1300px' }}>
-        <Logo />
+        <Navbar.Brand as={Link} to="/" className="logo my-0 pt-0">
+          <img
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Holidaze logo"
+          />
+          Holidaze
+        </Navbar.Brand>
         {user ? <UserNavbar /> : <UnAuthUi />}
       </Container>
     </Navbar>
