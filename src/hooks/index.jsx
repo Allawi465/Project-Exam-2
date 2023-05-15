ba177965c6359fa70cf0db3aaaae71870426dc81
Allawi <allawi465@gmail.com>
2023-05-15 14:25:06 +0200

    added Helmet as hook function

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -2,3 +2,4 @@ export { default as useGetApi } from './api/useGetApi';
 export { default as useApiActions } from './api/useApiActions';
 export { default as useModel } from './model';
 export { default as useCarousel } from './carousel';
+export { default as useHelmet } from './helmet';
