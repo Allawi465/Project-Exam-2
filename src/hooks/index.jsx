6f45134efbe748f70006d671d53eb138d5e419d7
Allawi <allawi465@gmail.com>
2023-05-18 17:50:03 +0200

    added use hook for calender

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -3,3 +3,4 @@ export { default as useApiActions } from './api/useApiActions';
 export { default as useModel } from './model';
 export { default as useCarousel } from './carousel';
 export { default as useHelmet } from './helmet';
+export { default as useBookingCalendar } from './calender';
