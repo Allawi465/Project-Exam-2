3a77eb19f67e4adbf016755714332b55fdcffe46
Allawi <allawi465@gmail.com>
2023-05-18 17:55:34 +0200

    added a booking page

--- src/pages/booking/index.jsx
+++ src/pages/booking/index.jsx
@@ -1,16 +1,59 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
+import { Link, useParams } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { useGetApi, useHelmet } from '../../hooks/index';
+import { Loading, ErrorMessage } from '../../components';
+import { BsChevronLeft } from 'react-icons/bs';
+import { Bookings } from '../../components';
 
 function Booking() {
+  let { id } = useParams();
+
+  const { data, isLoading, isError } = useGetApi(
+    `/venues/${id}?_owner=true&_bookings=true`
+  );
+
+  const title = data.name ? `book ${data.name} | Holidaze` : 'Book | Holidaze';
+
+  console.log(data);
+
+  const bookingsMeta = useHelmet({
+    title,
+    description: `book ${data.name} on Holidaze. View photos, read reviews, and check in the dates`,
+    keywords: 'Holidaze, venue, bookings, rentals',
+  });
+
+  if (isLoading) {
+    return (
+      <div className="d-flex justify-content-center mt-4">
+        <Loading />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="errorMessage">
+        <ErrorMessage
+          variant="danger"
+          text="we are aware of the issues with accessing Holidaze, our team is actively working on it."
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className="text-white">
-      <Helmet>
-        <title>Holidaze - 404 Page Not Found</title>
-        <meta name="description" content="Page not found. Please try again." />
-        <meta name="keywords" content="404, page not found, Holidaze" />
-      </Helmet>
-      <div className="text-black">Booking</div>
-    </div>
+    <>
+      {bookingsMeta}
+      <Container className="mt-3">
+        <div>
+          <Link className="return-Link" to={`/venue/${data.id}`}>
+            <BsChevronLeft className="mb-1 me-2" /> Return {data.name}
+          </Link>
+        </div>
+        <Bookings data={data} />
+      </Container>
+    </>
   );
 }
 
