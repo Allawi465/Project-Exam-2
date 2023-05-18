7cdf8a77dd71bb5ec4453a44e8123eed3f97286d
Allawi <allawi465@gmail.com>
2023-05-18 17:54:37 +0200

    added more style to bookings and cards

--- src/style/buttons.js
+++ src/style/buttons.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
 export const LoginBtn = styled.button`
-  background: ${(props) => props.theme.color.purple};
+  background: ${(props) => props.theme.color.lightBlack};
   padding: 0.375rem 0.75rem;
   color: #ffffff;
   border: 1px solid transparent;
+  text-decoration: none;
   line-height: 1.5;
+  display: flex;
   border-radius: 0.25rem;
+  justify-content: center;
+  align-items: center;
   font-size: 16px;
   transition: all 0.2s ease-in-out;
   :hover {
-    border: 1px solid ${(props) => props.theme.color.purple};
+    border: 1px solid ${(props) => props.theme.color.lightBlack};
     background: transparent;
     color: #000000;
   }
@@ -27,6 +31,7 @@ export const ModelBtn = styled(LoginBtn)`
 
 export const ModelLoadingBtn = styled(ModelBtn)`
   background: ${(props) => props.theme.color.purple};
+  width: 100%;
   :hover {
     background: ${(props) => props.theme.color.purple};
     color: #ffffff;
