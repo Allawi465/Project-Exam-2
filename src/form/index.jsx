54bbc09eb76a253bf8acb8cffc9bdb015eb86c12
Allawi <allawi465@gmail.com>
2023-05-23 23:36:59 +0200

    exported VenueMangerForm from form index files

--- src/form/index.jsx
+++ src/form/index.jsx
@@ -5,3 +5,4 @@ export { default as SearchForm } from './search';
 export { default as BookingForm } from './calender';
 export { default as CreateVenueForm } from './create';
 export { default as UpdateVenue } from './update';
+export { default as VenueMangerForm } from './venueManger';
