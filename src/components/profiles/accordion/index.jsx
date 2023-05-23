2c2751bcad8f839b60504a3edd7ec6e5a79383c7
Allawi <allawi465@gmail.com>
2023-05-23 23:44:03 +0200

    checking if the profiles match the loggedUserName before showing the option model and button

--- src/components/profiles/accordion/index.jsx
+++ src/components/profiles/accordion/index.jsx
@@ -1,21 +1,21 @@
 import Accordion from 'react-bootstrap/Accordion';
 import { YourBookings, YourVenues } from './ui';
 
-function AccordionProfile({ bookings, venues, venueManager }) {
+function AccordionProfile({ bookings, venues, venueManager, name, isLogName }) {
   return (
     <Accordion defaultActiveKey={['0']} flush alwaysOpen>
       <Accordion.Item eventKey="0">
         <Accordion.Header>Your bookings</Accordion.Header>
-        <YourBookings bookings={bookings} />
+        <YourBookings bookings={bookings} name={name} isLogName={isLogName} />
       </Accordion.Item>
-      {venueManager ? (
+      {venueManager === true && (
         <>
           <Accordion.Item eventKey="1">
             <Accordion.Header>Your venues</Accordion.Header>
-            <YourVenues venues={venues} />
+            <YourVenues venues={venues} name={name} isLogName={isLogName} />
           </Accordion.Item>
         </>
-      ) : null}
+      )}
     </Accordion>
   );
 }
