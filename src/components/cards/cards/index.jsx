18815dc9684ccb7c721d06f7a220514c474eb4cf
Allawi <allawi465@gmail.com>
2023-05-25 00:37:53 +0200

    added a filter venues with dates, number for guest and search query

--- src/components/cards/cards/index.jsx
+++ src/components/cards/cards/index.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
 import { Card, Col } from 'react-bootstrap';
 import { useGetApi } from '../../../hooks/index';
+import {
+  errorMessageDisplay,
+  VenuesFacilities,
+  FilterVenues,
+} from './ui/index';
 import { Loading, ErrorMessage } from '../../index';
 import VenuesCarousel from '../../carousel/cards';
-import VenuesFacilities from './facilities';
 import { AiFillHeart } from 'react-icons/ai';
 
 /**
@@ -30,12 +34,10 @@ import { AiFillHeart } from 'react-icons/ai';
  * <Venue data={props.data} />
  */
 
-function Venues() {
+function Venues({ selectedDates, guests, search }) {
   const { data, isLoading, isError } = useGetApi(
     `/venues?_owner=true&_bookings=true&limit=50&sort=created`
   );
-
-  console.log(data);
 
   if (isLoading) {
     return (
@@ -56,68 +58,80 @@ function Venues() {
     );
   }
 
+  const filteredVenues = FilterVenues(data, selectedDates, guests, search);
+
+  if (filteredVenues.length === 0) {
+    return (
+      <>
+        <ErrorMessage
+          variant="warning"
+          text={errorMessageDisplay(search, guests, selectedDates)}
+        />
+      </>
+    );
+  }
+
   return (
     <>
-      {data &&
-        data.map((venue) => (
-          <Col xs={12} sm={6} md={4} lg={4} key={venue.id}>
-            <Card className="mt-2 mb-4">
-              {venue.media && (
-                <VenuesCarousel media={venue.media} name={venue.name} />
-              )}
-              <Card.Body>
-                <div className="card-title mt-1 mb-0">
-                  <h5 className="card-title-title">{venue.name}</h5>
-                  {venue.rating > 0 && (
-                    <div className="venue-detail-rating">
-                      <AiFillHeart /> <span>{venue.rating}</span>
-                    </div>
+      {filteredVenues.map((venue) => (
+        <Col xs={12} sm={6} md={4} lg={4} key={venue.id}>
+          <Card className="mt-2 mb-4">
+            {venue.media && (
+              <VenuesCarousel media={venue.media} name={venue.name} />
+            )}
+            <Card.Body>
+              <div className="card-title mt-1 mb-0">
+                <h5 className="card-title-title">{venue.name}</h5>
+                {venue.rating > 0 && (
+                  <div className="venue-detail-rating">
+                    <AiFillHeart /> <span>{venue.rating}</span>
+                  </div>
+                )}
+              </div>
+              <div className="card-text">
+                <div className="card-text-address">
+                  {!venue.location.address ||
+                  venue.location.address === 'Unknown' ||
+                  !venue.location.city ||
+                  venue.location.city === 'Unknown' ? (
+                    <p>Unknown location</p>
+                  ) : (
+                    <p>
+                      {venue.location.address}, {venue.location.city}
+                    </p>
                   )}
                 </div>
-                <div className="card-text">
-                  <div className="card-text-address">
-                    {!venue.location.address ||
-                    venue.location.address === 'Unknown' ||
-                    !venue.location.city ||
-                    venue.location.city === 'Unknown' ? (
-                      <p>Unknown location</p>
-                    ) : (
-                      <p>
-                        {venue.location.address}, {venue.location.city}
-                      </p>
-                    )}
+                <div className="card-text-container">
+                  <VenuesFacilities
+                    breakfast={venue.meta.breakfast}
+                    wifi={venue.meta.wifi}
+                    pets={venue.meta.pets}
+                    parking={venue.meta.parking}
+                  />
+                </div>
+                <div className="card-text-prices">
+                  <div className="card-text-prices-container d-flex">
+                    <p className="card-text-prices-price">
+                      ${venue.price}
+                      <span className="card-text-prices-price-night">
+                        /Night
+                      </span>
+                    </p>
                   </div>
-                  <div className="card-text-container">
-                    <VenuesFacilities
-                      breakfast={venue.meta.breakfast}
-                      wifi={venue.meta.wifi}
-                      pets={venue.meta.pets}
-                      parking={venue.meta.parking}
-                    />
-                  </div>
-                  <div className="card-text-prices">
-                    <div className="card-text-prices-container d-flex">
-                      <p className="card-text-prices-price">
-                        ${venue.price}
-                        <span className="card-text-prices-price-night">
-                          /Night
-                        </span>
-                      </p>
-                    </div>
-                    <div className="me-1">
-                      <Link
-                        to={`/venue/${venue.id}`}
-                        className="card-text-prices-link stretched-link"
-                      >
-                        View
-                      </Link>
-                    </div>
+                  <div className="me-1">
+                    <Link
+                      to={`/venue/${venue.id}`}
+                      className="card-text-prices-link stretched-link"
+                    >
+                      View
+                    </Link>
                   </div>
                 </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
     </>
   );
 }
---
+++ src/components/cards/cards/ui/filterVenues.jsx
@@ -0,0 +1,28 @@
+import { eachDayOfInterval } from 'date-fns';
+
+function FilterVenues(venues, selectedDates, guests, search) {
+  const startDate = selectedDates ? new Date(selectedDates[0]) : null;
+  const endDate = selectedDates ? new Date(selectedDates[1]) : null;
+  const dates =
+    startDate && endDate
+      ? eachDayOfInterval({ start: startDate, end: endDate })
+      : [];
+  return venues.filter((venue) => {
+    return (
+      guests <= venue.maxGuests &&
+      !venue.bookings.some((booking) => {
+        const bookingStartDate = new Date(booking.dateFrom);
+        const bookingEndDate = new Date(booking.dateTo);
+        return dates.some((date) => {
+          return date >= bookingStartDate && date <= bookingEndDate;
+        });
+      }) &&
+      (venue.name.toLowerCase().includes(search.toLowerCase()) ||
+        venue.location.address.toLowerCase().includes(search.toLowerCase()) ||
+        venue.location.city.toLowerCase().includes(search.toLowerCase()))
+    );
+  });
+  
+}
+
+export default FilterVenues;
