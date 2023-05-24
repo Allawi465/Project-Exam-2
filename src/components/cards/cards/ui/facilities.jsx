ff247de93c5e950b392b9d80619d6adc8fc90232
Allawi <allawi465@gmail.com>
2023-05-25 00:36:38 +0200

    moved to own folder ui

---
+++ src/components/cards/cards/ui/facilities.jsx
@@ -0,0 +1,46 @@
+import { BsWifi } from 'react-icons/bs';
+import { GiKnifeFork } from 'react-icons/gi';
+import { MdPets } from 'react-icons/md';
+import { AiFillCar } from 'react-icons/ai';
+
+/**
+ * Component that render the home facilities of venues, including breakfast, WiFi, pets, and parking information
+ * @component
+ * @param {Object} props The component props
+ * @param {boolean} props.breakfast Indicates if the venue provides breakfast
+ * @param {boolean} props.wifi Indicates if the venue provides free WiFi
+ * @param {boolean} props.pets Indicates if pets are allowed in the venue
+ * @param {boolean} props.parking Indicates if free parking is available at the venue
+ * @returns {React.ReactElement} return VenuesFacilities component
+ * @example
+ * <VenuesFacilities breakfast={props.breakfast} wifi={props.wifi} pets={props.pets} parking={props.parking} />
+ */
+
+function VenuesFacilities({ breakfast, wifi, pets, parking }) {
+  return (
+    <div>
+      {breakfast && (
+        <div className="card-text-icon">
+          <GiKnifeFork />
+        </div>
+      )}
+      {wifi && (
+        <div className="card-text-icon ms-1">
+          <BsWifi />
+        </div>
+      )}
+      {pets && (
+        <div className="card-text-icon ms-1">
+          <MdPets />
+        </div>
+      )}
+      {parking && (
+        <div className="card-text-icon ms-1">
+          <AiFillCar />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default VenuesFacilities;
---
+++ src/components/cards/cards/ui/index.jsx
@@ -0,0 +1,3 @@
+export { default as errorMessageDisplay } from './errorsDisplay';
+export { default as VenuesFacilities } from './facilities';
+export { default as FilterVenues } from './filterVenues';
