dcc30b96e23e580bbc294311706e20f8abcb9d01
Allawi <allawi465@gmail.com>
2023-05-26 17:35:07 +0200

    removed Container from the jsx

--- src/components/profiles/accordion/ui/itemOne.jsx
+++ src/components/profiles/accordion/ui/itemOne.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Container, Col, Row, Accordion } from 'react-bootstrap';
+import { Col, Row, Accordion } from 'react-bootstrap';
 import { useModel } from '../../../../hooks/index';
 import { CancelBookingsModel } from '../../../index';
 import defaultImages from '../../../../images/default.jpg';
@@ -14,111 +14,104 @@ function YourBookings({ bookings, name, isLogName }) {
   return (
     <>
       <Accordion.Body className="profile-bookings">
-        <Container>
+        {bookings && bookings.length > 0 ? (
           <Row>
-            {bookings && bookings.length > 0 ? (
-              <Container>
-                <Row>
-                  {bookings.map((booking) => (
-                    <Col xs={12} sm={6} md={6} lg={4} key={booking.id}>
-                      <div
-                        to={`/venue/${booking.venue.id}`}
-                        className="profile-bookings-container mb-3"
-                      >
-                        <Link
-                          className="profile-bookings-container-img"
-                          to={`/venue/${booking.venue.id}`}
-                        >
-                          {booking.venue.media &&
-                          booking.venue.media.length > 0 ? (
-                            <img
-                              src={booking.venue.media[0]}
-                              alt={booking.venue.name}
-                              onError={(e) => {
-                                e.target.src = defaultImages;
-                              }}
-                            />
-                          ) : (
-                            <img
-                              src={defaultImages}
-                              alt={`${booking.venue.name} default`}
-                            />
-                          )}
-                        </Link>
-                        <div className="profile-bookings-container-info">
-                          <Link
-                            to={`/venue/${booking.venue.id}`}
-                            className="profile-bookings-container-info-link"
-                          >
-                            <h2 className="mb-2 h6 mt-0 profile-bookings-container-info-title">
-                              {booking.venue.name}
-                            </h2>
+            {bookings.map((booking) => (
+              <Col xs={12} sm={6} md={6} lg={4} key={booking.id}>
+                <div
+                  to={`/venue/${booking.venue.id}`}
+                  className="profile-bookings-container mb-3"
+                >
+                  <Link
+                    className="profile-bookings-container-img"
+                    to={`/venue/${booking.venue.id}`}
+                  >
+                    {booking.venue.media && booking.venue.media.length > 0 ? (
+                      <img
+                        src={booking.venue.media[0]}
+                        alt={booking.venue.name}
+                        onError={(e) => {
+                          e.target.src = defaultImages;
+                        }}
+                      />
+                    ) : (
+                      <img
+                        src={defaultImages}
+                        alt={`${booking.venue.name} default`}
+                      />
+                    )}
+                  </Link>
+                  <div className="profile-bookings-container-info">
+                    <Link
+                      to={`/venue/${booking.venue.id}`}
+                      className="profile-bookings-container-info-link"
+                    >
+                      <h2 className="mb-2 h6 mt-0 profile-bookings-container-info-title">
+                        {booking.venue.name}
+                      </h2>
 
-                            <div className="d-flex gap-2 mb-1">
-                              {booking.venue.rating > 0 && (
-                                <div className="venue-detail-rating">
-                                  <AiFillHeart className="me-1" />
-                                  <span>{booking.venue.rating}</span>
-                                </div>
-                              )}
-                              <div>
-                                <MdGroup className="me-1 mb-1" size={20} />
-                                <span>{booking.guests}</span>
-                              </div>
-                            </div>
-
-                            <span>
-                              <HiLocationMarker className="me-1 mb-1" />
-                              {!booking.venue.location.country ||
-                              booking.venue.location.country === 'Unknown' ||
-                              !booking.venue.location.city ||
-                              booking.venue.location.city === 'Unknown' ? (
-                                <span>Unknown location</span>
-                              ) : (
-                                <span>
-                                  {booking.venue.location.country},{' '}
-                                  {booking.venue.location.city}
-                                </span>
-                              )}
-                            </span>
-                            <div className="mt-1">
-                              <h3 className="h5">Date</h3>
-                              <p className="mb-0">
-                                {format(new Date(booking.dateFrom), 'dd.MM.yy')}{' '}
-                                - {format(new Date(booking.dateTo), 'dd.MM.yy')}
-                              </p>
-                            </div>
-                          </Link>
-                          {isLogName === name && (
-                            <div className="profile-bookings-container-btn mb-1">
-                              {new Date(booking.dateFrom) > new Date() && (
-                                <button
-                                  onClick={() => handleOpenDelete(booking.id)}
-                                  className="my-1 p-0"
-                                >
-                                  Cancel bookings
-                                </button>
-                              )}
-                            </div>
-                          )}
+                      <div className="d-flex gap-2 mb-1">
+                        {booking.venue.rating > 0 && (
+                          <div className="venue-detail-rating">
+                            <AiFillHeart className="me-1" />
+                            <span>{booking.venue.rating}</span>
+                          </div>
+                        )}
+                        <div>
+                          <MdGroup className="me-1 mb-1" size={20} />
+                          <span>{booking.guests}</span>
                         </div>
                       </div>
-                    </Col>
-                  ))}
-                </Row>
-              </Container>
-            ) : (
-              <p>No bookings yet...</p>
-            )}
+
+                      <span>
+                        <HiLocationMarker className="me-1 mb-1" />
+                        {!booking.venue.location.country ||
+                        booking.venue.location.country === 'Unknown' ||
+                        !booking.venue.location.city ||
+                        booking.venue.location.city === 'Unknown' ? (
+                          <span>Unknown location</span>
+                        ) : (
+                          <span>
+                            {booking.venue.location.country},{' '}
+                            {booking.venue.location.city}
+                          </span>
+                        )}
+                      </span>
+                      <div className="mt-1">
+                        <h3 className="h5">Date</h3>
+                        <p className="mb-0">
+                          {format(new Date(booking.dateFrom), 'dd.MM.yy')} -{' '}
+                          {format(new Date(booking.dateTo), 'dd.MM.yy')}
+                        </p>
+                      </div>
+                    </Link>
+                    {isLogName === name && (
+                      <div className="profile-bookings-container-btn mb-1">
+                        {new Date(booking.dateFrom) > new Date() && (
+                          <button
+                            onClick={() => handleOpenDelete(booking.id)}
+                            className="my-1 p-0"
+                          >
+                            Cancel bookings
+                          </button>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </Col>
+            ))}
           </Row>
-          {showDeleteModel && (
-            <CancelBookingsModel
-              show={showDeleteModel}
-              onClose={handleCloseDelete}
-              id={deleteId}
-            />
-          )}
-        </Container>
+        ) : (
+          <p>No bookings yet...</p>
+        )}
+        {showDeleteModel && (
+          <CancelBookingsModel
+            show={showDeleteModel}
+            onClose={handleCloseDelete}
+            id={deleteId}
+          />
+        )}
       </Accordion.Body>
     </>
   );
--- src/components/profiles/accordion/ui/itemTwo.jsx
+++ src/components/profiles/accordion/ui/itemTwo.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Container, Col, Row, Accordion } from 'react-bootstrap';
+import { Col, Row, Accordion } from 'react-bootstrap';
 import SettingsVenue from './dropdown';
 import defaultImages from '../../../../images/default.jpg';
 import { AiFillHeart } from 'react-icons/ai';
@@ -10,94 +10,84 @@ function YourVenues({ venues, isLogName, name }) {
   return (
     <>
       <Accordion.Body className="profile-bookings">
-        <Container>
+        {venues && venues.length > 0 ? (
           <Row>
-            {venues && venues.length > 0 ? (
-              <Container>
-                <Row>
-                  {venues.map((venue) => (
-                    <Col xs={12} sm={6} md={6} lg={4} key={venue.id}>
-                      <div
-                        to={`/venue/${venue.id}`}
-                        className="profile-bookings-container mb-3"
-                      >
-                        <Link
-                          className="profile-bookings-container-img"
-                          to={`/venue/${venue.id}`}
-                        >
-                          {venue.media && venue.media.length > 0 ? (
-                            <img
-                              src={venue.media[0]}
-                              alt={venue.name}
-                              onError={(e) => {
-                                e.target.src = defaultImages;
-                              }}
-                            />
-                          ) : (
-                            <img
-                              src={defaultImages}
-                              alt={`${venue.name} default`}
-                            />
-                          )}
-                        </Link>
-                        <div className="profile-bookings-container-info">
-                          <Link
-                            to={`/venue/${venue.id}`}
-                            className="profile-bookings-container-info-link"
-                          >
-                            <h2 className="mb-2 h6 mt-0 profile-bookings-container-info-title">
-                              {venue.name}
-                            </h2>
+            {venues.map((venue) => (
+              <Col xs={12} sm={6} md={6} lg={4} key={venue.id}>
+                <div
+                  to={`/venue/${venue.id}`}
+                  className="profile-bookings-container mb-3"
+                >
+                  <Link
+                    className="profile-bookings-container-img"
+                    to={`/venue/${venue.id}`}
+                  >
+                    {venue.media && venue.media.length > 0 ? (
+                      <img
+                        src={venue.media[0]}
+                        alt={venue.name}
+                        onError={(e) => {
+                          e.target.src = defaultImages;
+                        }}
+                      />
+                    ) : (
+                      <img src={defaultImages} alt={`${venue.name} default`} />
+                    )}
+                  </Link>
+                  <div className="profile-bookings-container-info">
+                    <Link
+                      to={`/venue/${venue.id}`}
+                      className="profile-bookings-container-info-link"
+                    >
+                      <h2 className="mb-2 h6 mt-0 profile-bookings-container-info-title">
+                        {venue.name}
+                      </h2>
 
-                            <div className="d-flex gap-2 mb-1">
-                              {venue.rating > 0 && (
-                                <div className="venue-detail-rating">
-                                  <AiFillHeart className="me-1" />
-                                  <span>{venue.rating}</span>
-                                </div>
-                              )}
-                              <div>
-                                <MdGroup className="me-1 mb-1" size={20} />
-                                <span>{venue.maxGuests}</span>
-                              </div>
-                            </div>
-                            <span>
-                              <HiLocationMarker className="me-1" />
-                              {!venue.location.country ||
-                              venue.location.country === 'Unknown' ||
-                              !venue.location.city ||
-                              venue.location.city === 'Unknown' ? (
-                                <span>Unknown location</span>
-                              ) : (
-                                <span>
-                                  {venue.location.country},{' '}
-                                  {venue.location.city}
-                                </span>
-                              )}
-                            </span>
-                            <div className="mt-2">
-                              <h3 className="h5">Created</h3>
-                              <p className="mb-0">
-                                {new Date(venue.created).toLocaleDateString()}
-                              </p>
-                            </div>
-                          </Link>
-                        </div>
-                        {isLogName === name && (
-                          <div className="profile-bookings-container-settings">
-                            <SettingsVenue id={venue.id} />
+                      <div className="d-flex gap-2 mb-1">
+                        {venue.rating > 0 && (
+                          <div className="venue-detail-rating">
+                            <AiFillHeart className="me-1" />
+                            <span>{venue.rating}</span>
                           </div>
                         )}
+                        <div>
+                          <MdGroup className="me-1 mb-1" size={20} />
+                          <span>{venue.maxGuests}</span>
+                        </div>
                       </div>
-                    </Col>
-                  ))}
-                </Row>
-              </Container>
-            ) : (
-              <p>No venues yet...</p>
-            )}
+                      <span>
+                        <HiLocationMarker className="me-1" />
+                        {!venue.location.country ||
+                        venue.location.country === 'Unknown' ||
+                        !venue.location.city ||
+                        venue.location.city === 'Unknown' ? (
+                          <span>Unknown location</span>
+                        ) : (
+                          <span>
+                            {venue.location.country}, {venue.location.city}
+                          </span>
+                        )}
+                      </span>
+                      <div className="mt-2">
+                        <h3 className="h5">Created</h3>
+                        <p className="mb-0">
+                          {new Date(venue.created).toLocaleDateString()}
+                        </p>
+                      </div>
+                    </Link>
+                  </div>
+                  {isLogName === name && (
+                    <div className="profile-bookings-container-settings">
+                      <SettingsVenue id={venue.id} />
+                    </div>
+                  )}
+                </div>
+              </Col>
+            ))}
           </Row>
-        </Container>
+        ) : (
+          <p>No venues yet...</p>
+        )}
       </Accordion.Body>
     </>
   );
