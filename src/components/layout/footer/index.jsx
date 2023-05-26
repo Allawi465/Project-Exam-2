7f5fdbf518a845dd13e587ab685c4c3e8b7146f0
Allawi <allawi465@gmail.com>
2023-05-26 17:33:45 +0200

    created footer components

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,10 +1,11 @@
 import { Container } from 'react-bootstrap';
+
 function Footer() {
   return (
-    <footer className="footer-style">
+    <footer className="footer mt-3">
       <Container>
-        <div className="footer-style-sec d-flex flex-wrap justify-content-center">
-          <p className="footer-style-p mt-2">&copy; 2023 BUYERS </p>
+        <div className="footer-links">
+          <p className="mt-2 mb-1">&copy; 2023 HOLIDAZE</p>
         </div>
       </Container>
     </footer>
