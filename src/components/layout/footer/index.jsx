20e5d40f83968b69f4716690f25c7d19697e0e44
Allawi <allawi465@gmail.com>
2023-05-26 14:12:53 +0200

    created footer

---
+++ src/components/layout/footer/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+function Footer() {
+  return (
+    <footer className="footer-style">
+      <Container>
+        <div className="footer-style-sec d-flex flex-wrap justify-content-center">
+          <p className="footer-style-p mt-2">&copy; 2023 BUYERS </p>
+        </div>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
