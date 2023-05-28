b703dba69f73358235775669eaf173ad3d52ff64
Allawi <allawi465@gmail.com>
2023-05-28 19:08:25 +0200

    added global Cypress to cypress components

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -13,6 +13,8 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+/* global Cypress */
+
 // Import commands.js using ES2015 syntax:
 import './commands';
 
