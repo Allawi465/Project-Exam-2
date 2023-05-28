1d70ec4b2d57fb8a0e0f1e9e031552891f52c30c
Allawi <allawi465@gmail.com>
2023-05-28 19:27:53 +0200

    added jsDocs

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -5,13 +5,7 @@ import { RenderDifferentContent } from './renderContent';
 /**
  * A component that render a hero section with a call-to-action to become a host.
  * @component
- * @property {function} useModel A custom hook that provides the functionality for showing the login and sign up modals
- * @property {boolean} showLoginModel A boolean that determines whether to show the login modal
- * @property {boolean} showSignUpModel A boolean that determines whether to show the sign up modal
- * @property {function} handleLoginModel A function to handle showing the login modal
- * @property {function} handleCloseLoginModel A function to handle closing the login modal
- * @property {function} handleSignUpModel A function to handle showing the sign up modal
- * @property {function} handleCloseSignUpModel A function to handle closing the sign up modal
+ * @property {function} RenderDifferentContent render when the user login
  * @returns {React.ReactElement} return Hero component
  * @example
  * <Hero />
--- src/components/hero/renderContent.jsx
+++ src/components/hero/renderContent.jsx
@@ -6,6 +6,25 @@ import { SignUpModel, LoginModel, VenueMangerModel } from '../index';
 import { AuthContext } from '../../api';
 import { Link } from 'react-router-dom';
 
+/**
+ * A component that renders different content when based user token and role
+ * @component
+ * @property {function} AuthContext getting the authentication state from AuthContext
+ * @property {function} load authentication state from local Storage
+ * @property {function} useModel A custom hook that provides the functionality for showing the login and sign up modals
+ * @property {boolean} showLoginModel A boolean that determines whether to show the login modal
+ * @property {boolean} showSignUpModel A boolean that determines whether to show the sign up modal
+ * @property {function} handleLoginModel A function to handle showing the login modal
+ * @property {function} handleCloseLoginModel A function to handle closing the login modal
+ * @property {function} handleSignUpModel A function to handle showing the sign up modal
+ * @property {function} handleCloseSignUpModel A function to handle closing the sign up modal
+ * @property {function} handleCloseVenueManger A function to handle closing the setting VenueManger modal
+ * @property {function} handleOpenVenueManger A function to handle showing the setting VenueManger modal
+ * @returns {React.ReactElement} return Hero component
+ * @example
+ * <Hero />
+ */
+
 export function RenderDifferentContent() {
   const { dataLogin } = useContext(AuthContext);
   const {
@@ -15,9 +34,9 @@ export function RenderDifferentContent() {
     handleCloseLoginModel,
     handleSignUpModel,
     handleCloseSignUpModel,
-    showSettings,
-    handleCloseSettings,
-    handleOpenSettings,
+    handleCloseVenueManger,
+    showVenueManger,
+    handleOpenVenueManger,
   } = useModel();
   const user = dataLogin ? dataLogin : load('user');
   const venueManger =
@@ -59,8 +78,11 @@ export function RenderDifferentContent() {
       <div className="hero-text-container">
         <h1>Want to host your own place?</h1>
         <p>Earn passive income by renting properties!</p>
-        <HeroBtn onClick={handleOpenSettings}>Become a host</HeroBtn>
-        <VenueMangerModel show={showSettings} onClose={handleCloseSettings} />
+        <HeroBtn onClick={handleOpenVenueManger}>Become a host</HeroBtn>
+        <VenueMangerModel
+          show={showVenueManger}
+          onClose={handleCloseVenueManger}
+        />
       </div>
     );
   }
