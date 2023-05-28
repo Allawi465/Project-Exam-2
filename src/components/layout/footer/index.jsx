d1b5eeab9bd5f4ccf81869a9a7071714097d8695
Allawi <allawi465@gmail.com>
2023-05-28 19:29:10 +0200

    created a footer components

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,5 +1,14 @@
 import { Container } from 'react-bootstrap';
 
+/**
+ * The Footer component render the footer
+ * @function
+ * @property {function} NavBar render the navbar component
+ * @returns {React.ReactElement} return Footer component
+ * @example
+ *   <Footer />
+ */
+
 function Footer() {
   return (
     <footer className="footer mt-3">
